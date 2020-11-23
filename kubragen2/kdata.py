524f9964beae5ada0c27e53714d52498084fb11d
Rangel Reale <rangelspam@gmail.com>
2020-11-23 15:37:59 -0300

    kdata

--- kubragen2/kdata.py
+++ kubragen2/kdata.py
@@ -1,10 +1,75 @@
-from kubragen2.data import Data
+from typing import Optional, Mapping, Any
+
+from .data import Data
+from .exception import InvalidOperationError
 
 
 class KData(Data):
-    pass
+    def is_enabled(self) -> bool:
+        return True
+
+    def get_value(self) -> Any:
+        raise InvalidOperationError('Cannot get value of KData')
 
 
-# class KData_ConfigFile(KData):
-#     def __init__(self):
-#         pass
+class KData_Manual(KData):
+    """
+    A :class:`KData` that merges value manually.
+
+    :param merge_value: A Mapping to merge on the result.
+    """
+    merge_value: Optional[Mapping[Any, Any]]
+
+    def __init__(self, merge_value: Optional[Mapping[Any, Any]] = None):
+        self.merge_value = merge_value
+
+
+class KData_Value(KData):
+    """
+    A :class:`KData` with a constant value.
+
+    :param value: the data value
+    """
+    value: str
+
+    def __init__(self, value: Any):
+        self.value = value
+
+
+class KData_ConfigMap(KData):
+    """
+    A :class:`KData` that represents a Kubernetes ConfigMap item.
+
+    :param configmapName: ConfigMap name
+    :param configmapData: ConfigMap data name
+    """
+    configmapName: str
+    configmapData: str
+
+    def __init__(self, configmapName: str, configmapData: str):
+        self.configmapName = configmapName
+        self.configmapData = configmapData
+
+
+class KData_Secret(KData):
+    """
+    A :class:`KData` that represents a Kubernetes Secret item.
+
+    :param secretName: Secret name
+    :param secretData: Secret data name
+    """
+    secretName: str
+    secretData: str
+
+    def __init__(self, secretName: str, secretData: str):
+        self.secretName = secretName
+        self.secretData = secretData
+
+
+class KData_Env(KData):
+    name: str
+    value: Any
+
+    def __init__(self, name: str, value: Any):
+        self.name = name
+        self.value = value
--- kubragen2/kdatahelper.py
+++ kubragen2/kdatahelper.py
@@ -1,8 +1,10 @@
-from typing import Sequence, Any
+from typing import Sequence, Any, Optional, Mapping, List
 
 from .configfile import ConfigFileRender, ConfigFile, ConfigFileRenderMulti
-from .data import Data
+from .data import Data, DisabledData
 from .exception import InvalidParamError
+from .kdata import KData, KData_Value, KData_ConfigMap, KData_Secret, KData_Manual, KData_Env
+from .merger import merger
 from .options import Options
 
 
@@ -21,3 +23,89 @@ class KDataHelper_ConfigFile(KDataHelper):
         if isinstance(value, Data):
             return value
         raise InvalidParamError('Invalid parameter for config file: "{}"'.format(repr(value)))
+
+
+class KDataHelper_Env(KDataHelper):
+    """
+    KData helpers for Kubernetes *container.env* values.
+    """
+    @staticmethod
+    def info(base_value: Optional[Any] = None, value: Optional[Any] = None,
+             default_value: Optional[Any] = None, enabled: bool = True, disable_if_none: bool = False) -> Any:
+        """
+        Outputs a configuration compatible with the Kubernetes *container.env*.
+
+        If *value* is not a Mapping, it will be considered a simple 'value' field.
+
+        :param base_value: the base dict that is merged with the result, normally containing the name of the object.
+        :type base_value: Mapping
+        :param value: a value configured by the user, possibly None
+        :param value_if_kdata: a :class:`kubragen.kdata.KData` value configured by the user, possibly None. If
+            not a KData instance, **it will be ignored**, and you are supposed to set a value in *default_value.
+        :param default_value: a default value to use if value is None
+        :param enabled: whether the information is enabled. If not, a :class:`kubragen.data.DisabledData` is returned
+        :param disable_if_none: automatically disable if value and value_if_kdata is None
+        :return: a configuration compatible with the Kubernetes *container.env* specification
+        """
+        if not enabled:
+            return DisabledData()
+
+        ret = base_value
+        if ret is None:
+            ret = {}
+
+        if isinstance(value, KData_Env):
+            ret = {
+                'name': value.name,
+            }
+            value = value.value
+
+        if isinstance(value, KData):
+            if isinstance(value, KData_Manual):
+                default_value = merger.merge(ret, value.merge_value)
+            elif isinstance(value, KData_Value):
+                default_value = {
+                    'value': value.value,
+                }
+            elif isinstance(value, KData_ConfigMap):
+                default_value = {
+                    'valueFrom': {
+                        'configMapKeyRef': {
+                            'name': value.configmapName,
+                            'key': value.configmapData
+                        }
+                    },
+                }
+            elif isinstance(value, KData_Secret):
+                default_value = {
+                    'valueFrom': {
+                        'secretKeyRef': {
+                            'name': value.secretName,
+                            'key': value.secretData
+                        }
+                    },
+                }
+            else:
+                raise InvalidParamError('Unsupported KData: "{}"'.format(repr(value)))
+        elif value is not None:
+            if isinstance(value, Mapping):
+                default_value = value
+            else:
+                default_value = {
+                    'value': value,
+                }
+
+        # Check again
+        # if disable_if_none and default_value is None:
+        #     return DisabledData()
+
+        return merger.merge(ret, default_value)
+
+    @staticmethod
+    def list(value: Optional[Sequence[Any]] = None) -> Sequence[Any]:
+        ret: List[Any] = []
+        if value is None:
+            return ret
+        for v in value:
+            ret.append(KDataHelper_Env.info(base_value={}, value=v))
+        return ret
