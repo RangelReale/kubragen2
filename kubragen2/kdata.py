17fa0cb438476dfa389ccb0defdf82521a73bccb
Rangel Reale <rangelspam@gmail.com>
2020-11-23 16:27:37 -0300

    kdata

--- kubragen2/kdata.py
+++ kubragen2/kdata.py
@@ -2,6 +2,7 @@ from typing import Optional, Mapping, Any
 
 from .data import Data
 from .exception import InvalidOperationError
+from .merger import merger
 
 
 class KData(Data):
@@ -73,3 +74,119 @@ class KData_Env(KData):
     def __init__(self, name: str, value: Any):
         self.name = name
         self.value = value
+
+
+class KData_StorageClass(KData):
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def get_value(self) -> Any:
+        return {
+            'apiVersion': 'storage.k8s.io/v1',
+            'kind': 'StorageClass',
+            'metadata': {
+                'name': self.name,
+            },
+        }
+
+
+class KData_PersistentVolume(KData):
+    name: str
+    merge_config: Optional[Any]
+
+    def __init__(self, name: str, merge_config: Optional[Any] = None):
+        self.name = name
+        self.merge_config = merge_config
+
+    def get_value(self) -> Any:
+        return merger.merge(self.build(), self.merge_config if self.merge_config is not None else {})
+
+    def build(self) -> Any:
+        return {
+            'apiVersion': 'v1',
+            'kind': 'PersistentVolume',
+            'metadata': {
+                'name': self.name,
+            },
+            'spec': {
+            },
+        }
+
+
+class KData_PersistentVolume_EmptyDir(KData_PersistentVolume):
+    def build(self) -> Any:
+        return merger.merge(super().build(), {
+            'spec': {
+                'emptyDir': {}
+            },
+        })
+
+
+class KData_PersistentVolume_HostPath(KData_PersistentVolume):
+    hostpath: str
+
+    def __init__(self, name: str, hostpath: str, merge_config: Optional[Any] = None):
+        super().__init__(name=name, merge_config=merge_config)
+        self.hostpath = hostpath
+
+    def build(self) -> Any:
+        return merger.merge(super().build(), {
+            'spec': {
+                'hostPath': self.hostpath
+            },
+        })
+
+
+class KData_PersistentVolume_NFS(KData_PersistentVolume):
+    nfs: Any
+
+    def __init__(self, name: str, nfs: Any, merge_config: Optional[Any] = None):
+        super().__init__(name=name, merge_config=merge_config)
+        self.nfs = nfs
+
+    def build(self) -> Any:
+        return merger.merge(super().build(), {
+            'spec': {
+                'nfs': self.nfs
+            },
+        })
+
+
+class KData_PersistentVolume_CSI(KData_PersistentVolume):
+    csi: Any
+
+    def __init__(self, name: str, csi: Any, merge_config: Optional[Any] = None):
+        super().__init__(name=name, merge_config=merge_config)
+        self.csi = csi
+
+    def build(self) -> Any:
+        return merger.merge(super().build(), {
+            'spec': {
+                'csi': self.csi
+            },
+        })
+
+
+class KData_PersistentVolumeClaim(KData):
+    name: str
+    merge_config: Optional[Any]
+
+    def __init__(self, name: str, merge_config: Optional[Any] = None):
+        self.name = name
+        self.merge_config = merge_config
+
+    def get_value(self) -> Any:
+        return merger.merge(self.build(), self.merge_config if self.merge_config is not None else {})
+
+    def build(self) -> Any:
+        return {
+            'apiVersion': 'v1',
+            'kind': 'PersistentVolumeClaim',
+            'metadata': {
+                'name': self.name,
+            },
+            'spec': {
+            },
+        }
--- kubragen2/kdatahelper.py
+++ kubragen2/kdatahelper.py
@@ -109,3 +109,72 @@ class KDataHelper_Env(KDataHelper):
         for v in value:
             ret.append(KDataHelper_Env.info(base_value={}, value=v))
         return ret
+
+
+class KDataHelper_Volume(KDataHelper):
+    """
+    KData helpers for Kubernetes *podSpec.volumes* values.
+    """
+    @staticmethod
+    def info(base_value: Optional[Any] = None, value: Optional[Any] = None,
+             default_value: Optional[Any] = None, key_path: Optional[str] = None, enabled: bool = True,
+             disable_if_none: bool = False):
+        """
+        Outputs a configuration compatible with the Kubernetes *podSpec.volume* for the passed :class:`KData`,
+        with a default value if it was not configured by the user.
+
+        :param base_value: the base dict that is merged with the result, normally containing the name of the object.
+        :type base_value: dict
+        :param value: a value configured by the user, possibly None
+        :param value_if_kdata: a :class:`kubragen.kdata.KData` value configured by the user, possibly None. If
+            not a KData instance, **it will be ignored**, and you are supposed to set a value in *default_value.
+        :param default_value: a default value to use if value is None
+        :param enabled: whether the information is enabled. If not, a :class:`kubragen.data.DisabledData` is returned
+        :param disable_if_none: automatically disable if value and value_if_kdata is None
+        :return: a configuration compatible with the Kubernetes *podSpec.volume* specification
+        """
+        if not enabled:
+            return DisabledData()
+
+        ret = base_value
+        if ret is None:
+            ret = {}
+
+        if isinstance(value, KData):
+            if isinstance(value, KData_Manual):
+                default_value = merger.merge(ret, value.merge_value)
+            elif isinstance(value, KData_Value):
+                default_value = value.value
+            elif isinstance(value, KData_ConfigMap):
+                default_value = {
+                    'configMap': {
+                        'name': value.configmapName,
+                        'items': [{
+                            'key': value.configmapData,
+                            'path': value.configmapData if key_path is None else key_path,
+                        }],
+                    }
+                }
+            elif isinstance(value, KData_Secret):
+                default_value = {
+                    'secret': {
+                        'secretName': value.secretName,
+                        'items': [{
+                            'key': value.secretData,
+                            'path': value.secretData if key_path is None else key_path,
+                        }],
+                    }
+                }
+            else:
+                raise InvalidParamError('Unsupported KData: "{}"'.format(repr(value)))
+        elif value is not None:
+            if isinstance(value, Mapping):
+                default_value = value
+            else:
+                raise InvalidParamError('Unsupported Volume spec: "{}"'.format(str(value)))
+
+        # Check again
+        # if disable_if_none and default_value is None:
+        #     return DisabledData()
+
+        return merger.merge(ret, default_value)
